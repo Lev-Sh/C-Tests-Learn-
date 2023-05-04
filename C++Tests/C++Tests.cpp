678d6860969755f6ad5a40c5ebdfb6c5ede23cd4
Lev-Sh <65592917+Lev-Sh@users.noreply.github.com>
2023-05-04 18:02:02 +0300

    Добавьте файлы проекта.

---
+++ C++Tests/C++Tests.cpp
@@ -0,0 +1,78 @@
+﻿// C++Tests.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+
+
+void Append(std::string res, char c) {
+	if (res.back() != c) {
+		res.push_back(c);
+	}
+}
+std::string Soundex(const std::string word){
+	std::string res;
+	res.push_back(word[0]);
+	for (size_t i = 1; i != word.size(); ++i)
+	{
+		char l = word[i];
+		switch (l)
+		{
+            case 'b':
+            case 'f':
+            case 'p':
+            case 'v':
+                Append(res, '1');
+                break;
+            case 'c':
+            case 'g':
+            case 'j':
+            case 'k':
+            case 'q':
+            case 's':
+            case 'x':
+            case 'z':
+                Append(res, '2');
+                break;
+            case 'd':
+            case 't':
+                Append(res, '3');
+                break;
+            case 'l':
+                Append(res, '4');
+                break;
+            case 'm':
+            case 'n':
+                Append(res, '5');
+                break;
+            case 'r':
+                Append(res, '6');
+                break;
+		}
+        while (res.size() < 4) {
+            res.push_back('0');
+        }
+        res.resize(4);
+        return res;
+	}
+}
+
+int main() {
+    std::string word;
+    std::cin >> word;
+    std::cout << Soundex(word) << "\n";
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
